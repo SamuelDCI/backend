9505b73f351ae4f0e2734fd2b39288730eb97a71
Sam <sam234@yahoo.com>
2023-11-27 10:31:25 +0100

    hello

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -63,11 +63,8 @@ export const register = async (req, res, next) => {
     const hashedPassword = await bcrypt.hash(req.body.password,salt) */
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     console.log(hashedPassword);
-
-    const newUser = await User.create({
-      ...req.body,
-      password: hashedPassword,
-    });
+    req.body.password = hashedPassword;
+    const newUser = await User.create(req.body);
 
     res.status(200).json(newUser);
   } catch (error) {
